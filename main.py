2b63dad7ade4c429ba769911a2fe8cfa93c38f58
elizaveta-p <75022621+elizaveta-p@users.noreply.github.com>
2021-02-13 00:35:08 +0300

    new colors

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from random import randint
+from UI import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main1.ui', self)
+        # uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.w = SecondWindow()
 
@@ -30,7 +34,7 @@ class SecondWindow(QWidget):
 
     def draw(self, qp):
         r = randint(1, 400)
-        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        qp.setBrush(QBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)), Qt.SolidPattern))
         qp.drawEllipse(randint(0, 800 - r), randint(0, 800 - r), r, r)
 
 
